f2cf2f1dbc8ca5ea136d307d0c16c93ab2549a4f
abbyjo <abbyjo225@gmail.com>
2023-07-30 16:39:55 -0400

    Added function to save events upon clicking save button

--- script.js
+++ script.js
@@ -4,7 +4,10 @@
 $(function () {
   /* TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage.
   HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage? */
-  
+  var timeBlocks = $('#time-blocks').children();
+  timeBlocks.on("click",".saveBtn", function(){
+    localStorage.setItem($(this).parent().attr("id"), $(this).parent().children().eq(1).val())
+  } )
 
 
   /* TODO: Add code to apply the past, present, or future class to each time
@@ -12,8 +15,7 @@ $(function () {
   HINTS: How can the id attribute of each time-block be used to conditionally add or remove the
   past, present, and future classes? How can Day.js be used to get the
   current hour in 24-hour time? */
- let currentTime = dayjs().format('HA');
- let timeBlocks = $('#time-blocks').children();
+ var currentTime = dayjs().format('HA');
  timeBlocks.each(function(){
     if ($(this).attr("id") === currentTime){$(this).addClass("present")}
     else if ($(this).attr("id")> currentTime) {$(this).addClass("future")}  
